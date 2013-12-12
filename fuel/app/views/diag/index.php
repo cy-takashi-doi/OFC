1a294e5b21989fdd73c37d249b2b85ace3a0b34f
takashi_doi <takashi_doi@cybird.co.jp>
2013-12-12 19:29:09 +0900

    いいねボタンの設置

--- fuel/app/views/diag/index.php
+++ fuel/app/views/diag/index.php
@@ -11,10 +11,11 @@
    FB.Canvas.setAutoGrow();
   }
 </script>-->
+<div class="fb-like" data-href="https://www.facebook.com/pages/%E3%82%A4%E3%82%B1%E3%83%A1%E3%83%B3%E8%A8%BA%E6%96%AD%E3%83%86%E3%82%B9%E3%83%88-Community/224073727774189" data-width="450" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
 <div class="fb-container">
 	<?php echo Asset::img('background.png',array("width" => "810")); ?>
 	<div class="jumbotron">
-		<h1>イケメンとの相性を診断しよう！！</h1>
+		<h1>イケメン相性診断！！</h1>
 		<p>いいねをすると"イケメン診断"できるよ！！</p>
 		<p><a class="btn btn-primary btn-lg" role="button">今すぐ診断開始</a></p>
 	</div>
@@ -25,3 +26,11 @@
 	FB.Canvas.setAutoResize();
 	FB.Canvas.setSize({ width: Fluid, height: Fluid });
 </script>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/ja_JP/all.js#xfbml=1&appId=1382564888634899";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
