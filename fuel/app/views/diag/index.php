b643238a5adebe087e424eefe0b7b286e715db58
takashi_doi <takashi_doi@cybird.co.jp>
2013-12-12 20:11:00 +0900

    背景を半透明に変更

--- fuel/app/views/diag/index.php
+++ fuel/app/views/diag/index.php
@@ -1,6 +1,6 @@
 <!--<div class="fb-container">-->
-	<?php echo Asset::img('background.png',array("width" => "810")); ?>
-	<div class="jumbotron">
+	<!--<?php echo Asset::img('background.png',array("width" => "810")); ?>-->
+	<div class="jumbotron fb-jumbotron">
 		<h2>イケメン相性診断！！</h2>
 		<p>いいねをすると"イケメン診断"できるよ！！</p>
 		<div class="fb-like" data-href="https://www.facebook.com/pages/%E3%82%A4%E3%82%B1%E3%83%A1%E3%83%B3%E8%A8%BA%E6%96%AD%E3%83%86%E3%82%B9%E3%83%88-Community/224073727774189" data-width="450" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
