eca44e4eb8adeea6cc2ac69c5e70d2d7d3233eda
takashi_doi <takashi_doi@cybird.co.jp>
2013-12-12 20:53:02 +0900

    いいね後のリダイレクト追加

--- fuel/app/views/diag/index.php
+++ fuel/app/views/diag/index.php
@@ -14,10 +14,19 @@
 	FB.Canvas.setSize({ width: Fluid, height: Fluid });
 </script>
 <div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/ja_JP/all.js#xfbml=1&appId=1382564888634899";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+<script>
+$(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/ja_JP/all.js#xfbml=1&appId=1382564888634899";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+$(function() {
+	FB.Event.subscribe('edge.create',
+		function(response) {
+			parent.location.href = "/diag/start";
+		}
+	);
+});
+</script>
